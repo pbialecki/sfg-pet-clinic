c1cbb56a9a6e7f7b6ad257503f28b13b5afd9123
Paweł Białecki <pawel.bialecki@enterprisesoftware.pl>
2018-11-21 20:13:27 +0100

    Add Id generation to Map

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -24,25 +24,21 @@ public class DataLoader implements CommandLineRunner{
     @Override
     public void run(String... strings) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Michael");
         owner1.setLastName("Jackson");
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Tom");
         owner2.setLastName("Cruise");
         ownerService.save(owner2);
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("John");
         vet1.setLastName("Thompson");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Nico");
         vet2.setLastName("Hulkenberg");
         vetService.save(vet2);
